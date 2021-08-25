ab45807752efef99fde1a14b927443f15d3e72da
Leandro Ferreira <ldrSiqueira@gmail.com>
2021-08-25 11:23:11 -0300

    feat: apagado comentarios e libs nao usadas

--- services/api.js
+++ services/api.js
@@ -1,11 +1,4 @@
 import axios from 'axios';
-import {parseCookies} from "nookies";
-
-// const { 'whydo-token': token } = parseCookies()
 export const api = axios.create({
   baseURL: 'https://inlive.ga/',
 });
-// console.log(token)
-// if (true) {
-//   api.defaults.headers['Authorization'] = `Bearer ${token}`
-// }
