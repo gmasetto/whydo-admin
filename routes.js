62b4513ae745110cd2e1f009d65a5fb2180250f0
Leandro Ferreira <ldrSiqueira@gmail.com>
2021-08-25 12:01:42 -0300

    adicinado libs e alteracoes nas estruturas

--- routes.js
+++ routes.js
@@ -6,40 +6,52 @@ var routes = [
     layout: "/admin",
   },
   {
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
+    path: "/userTable",
+    name: "Tabela de Usuários",
+    icon: "ni ni-bullet-list-67 text-red",
     layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    layout: "/admin",
-  },
-  {
-    path: "/profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 text-yellow",
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Tables",
+  },{
+    path: "/clicksTable",
+    name: "Tabela de Clicks",
     icon: "ni ni-bullet-list-67 text-red",
     layout: "/admin",
   },
-  {
-    path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
-    layout: "/auth",
-  },
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    layout: "/auth",
-  },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: "ni ni-planet text-blue",
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: "ni ni-pin-3 text-orange",
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/profile",
+  //   name: "User Profile",
+  //   icon: "ni ni-single-02 text-yellow",
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/tables",
+  //   name: "Tables",
+  //   icon: "ni ni-bullet-list-67 text-red",
+  //   layout: "/admin",
+  // },
+
+  // {
+  //   path: "/login",
+  //   name: "Login",
+  //   icon: "ni ni-key-25 text-info",
+  //   layout: "/auth",
+  // },
+  // {
+  //   path: "/register",
+  //   name: "Register",
+  //   icon: "ni ni-circle-08 text-pink",
+  //   layout: "/auth",
+  // },
 ];
 export default routes;
